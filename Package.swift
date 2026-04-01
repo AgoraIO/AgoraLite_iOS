59576b4a868f479c82bf4ee2c3562b4c519219da
Ren Jingui <renjingui@agora.io>
2026-04-01 08:08:57 +0000

    4.5.3

--- Package.swift
+++ Package.swift
@@ -16,23 +16,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AgoraRtcKit",
-            url: "https://download.agora.io/swiftpm/AgoraLite_iOS/4.5.3.test/AgoraRtcKit.xcframework.zip",
-            checksum: "1da2db18c9f0d2ccac7b7e53e36170c51ed0258eca28d9aa95724f7dbeee882f"
+            url: "https://download.agora.io/swiftpm/AgoraLite_iOS/4.5.3/AgoraRtcKit.xcframework.zip",
+            checksum: "0671d467a6da49bcabc156064a02542733d121ad30034d2eab3bcb2120f99c65"
         ),
         .binaryTarget(
             name: "Agoraffmpeg",
-            url: "https://download.agora.io/swiftpm/AgoraLite_iOS/4.5.3.test/Agoraffmpeg.xcframework.zip",
-            checksum: "eaf83ec9f06fe9b6889ab5d34664a5ef926b2100211bc3fb89828dbc32493836"
+            url: "https://download.agora.io/swiftpm/AgoraLite_iOS/4.5.3/Agoraffmpeg.xcframework.zip",
+            checksum: "0ab0e884c95bc767d521d74d538be843da62bebdd7c143fbedfd1fd386229fd1"
         ),
         .binaryTarget(
             name: "AgoraSoundTouch",
-            url: "https://download.agora.io/swiftpm/AgoraLite_iOS/4.5.3.test/AgoraSoundTouch.xcframework.zip",
-            checksum: "9830a16403e403acbf3efcc0075536fb5df77f4ad15d5e99cd28db91a363ade1"
+            url: "https://download.agora.io/swiftpm/AgoraLite_iOS/4.5.3/AgoraSoundTouch.xcframework.zip",
+            checksum: "01e7806850c1cf43757caf1a486d578d99f594709e1c2851999c3f472711d6b7"
         ),
         .binaryTarget(
             name: "video_dec",
-            url: "https://download.agora.io/swiftpm/AgoraLite_iOS/4.5.3.test/video_dec.xcframework.zip",
-            checksum: "c6ab5d17a3e5999cd21edc7a4c783079643a2a220adc85a3525d30c44a8a5125"
+            url: "https://download.agora.io/swiftpm/AgoraLite_iOS/4.5.3/video_dec.xcframework.zip",
+            checksum: "41c6d08339d2d90dd419a0cbfc6d8f6ffa66d876765c27c62a3d1b34577ed3f1"
         ),
         .target(
             name: "AgoraInfra_iOS",
